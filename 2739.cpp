9836e13d4ebaec5ecbecda19e8c3d3b4719c466d
zyhwan <145274494+zyhwan@users.noreply.github.com>
2026-03-30 21:30:06 +0900

    반복문 2739

---
+++ 2739.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int num = 0;
+
+	std::cin >> num;
+
+	for (int i = 1; i < 10; ++i)
+	{
+		cout << num << " * " << i << " = ";
+
+		cout << num * i << endl;
+	}
+
+
+	return 0;
+}
