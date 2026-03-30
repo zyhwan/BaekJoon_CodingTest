11a5aa2724217f5c6f37d7e324be640b06c66b57
zyhwan <145274494+zyhwan@users.noreply.github.com>
2026-03-30 21:57:29 +0900

    반복문 25314

---
+++ 25314.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	int N = 0;
+	cin >> N;
+
+	int T = 0;
+	T = N / 4;
+
+	for (int i = 0; i < T; ++i)
+	{
+		cout << "long ";
+	}
+
+	cout << "int";
+
+
+	return 0;
+}
